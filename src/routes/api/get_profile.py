4c934d8fa6c47a033b2aa4fe61ef0c7246b7908c
julpr833 <julianpretto16@gmail.com>
2025-10-14 11:58:24 -0300

    fix faulty query asking for now non-existent attribute

--- src/routes/api/get_profile.py
+++ src/routes/api/get_profile.py
@@ -23,7 +23,7 @@ def get_profile(username):
     
     # Traer el perfil desde la base de datos
     query = """
-    SELECT `Username`, `Correo`, `Avatar_URL`, `FechaRegistro`, `PuntosTotales`, `Vetado`
+    SELECT `Username`, `Correo`, `Avatar_URL`, `FechaRegistro`, `Vetado`
     FROM `USUARIO` 
     WHERE `Username` = %s
     """
@@ -37,8 +37,7 @@ def get_profile(username):
             "email": userinfo[1],
             "avatar_url": userinfo[2],
             "registration_date": userinfo[3],
-            "total_points": userinfo[4],
-            "banned": userinfo[5]
+            "banned": userinfo[4]
         }
     
     return jsonify(userinfo)
