65d3512d4fda6429400e660fafe719c1baaa0e9e
julpr833 <julianpretto16@gmail.com>
2025-10-14 13:50:32 -0300

    add route for editing words

---
+++ src/routes/api/words/edit_word.py
@@ -0,0 +1,34 @@
+from flask import request
+from src.routes import api
+from src.middleware.auth import auth_required
+from src.util.validator import Validator
+from src.lib.database import mysql
+
+@api.route('/words/edit-word', methods=['PATCH'])
+@auth_required(level=2)
+def edit_word(username):
+    data = request.form
+    
+    word_id = data.get('word_id', None)
+    new_word = data.get('new_word', None)
+    
+    # La funcion word_exists_id valida que la palabra exista
+    # En caso de que exista, la devuelve, de lo contrario devuelve False
+    validator = Validator()
+    word = validator.word_exists_id(word_id)
+    
+    # Validamos la nueva palabra
+    if not validator.is_valid_word(new_word):
+        return validator.get_errors(), 400
+    
+    # En caso de que no exista, como va a ser False simplemente retornamos error
+    # El chequeo de word_id is None es un poco de overengineering pero bueno...
+    if word_id is None or word is False:
+        return {"error": "Palabra no encontrada"}, 404
+    
+    # Eliminamos la palabra
+    with mysql.get_db().cursor() as cursor:
+        cursor.execute("UPDATE `PALABRA` SET `Palabra` = %s WHERE `ID_Palabra` = %s", (new_word, word_id))
+        mysql.get_db().commit()
+        
+    return {"success": f"La palabra {word} fue actualizada."}, 201
