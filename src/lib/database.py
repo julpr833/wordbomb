1909d71852375a7c49897faacfcb81a5e14b9b0e
julpr833 <julianpretto16@gmail.com>
2025-10-15 18:48:51 -0300

    added get user id helper

--- src/lib/database.py
+++ src/lib/database.py
@@ -1,3 +1,11 @@
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
+
+def get_user_id(username: str) -> bool | int:
+    with mysql.get_db().cursor() as cursor:
+        cursor.execute("SELECT `ID_Usuario` FROM `USUARIO` WHERE `Username` = %s", (username))
+        result = cursor.fetchone()
+        if result is None:
+            return False
+        return result[0]
