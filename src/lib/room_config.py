0a5d7ae3638338983ce6692b39c44aea06c76a43
julpr833 <julianpretto16@gmail.com>
2025-10-20 12:49:38 -0300

    add roles and room config enums

---
+++ src/lib/room_config.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class Gamemodes(Enum):
+    CLASSIC = 1,
+    REVERSED = 2,
+    HARDCORE = 3
+    
+class Difficulty(Enum):
+    EASY = 1,
+    NORMAL = 2,
+    HARD = 3
+    
+class RoomStatus(Enum):
+    WAITING = 1,
+    PLAYING = 2
